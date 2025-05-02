659d3c2afc2c9079d14989dea22d59eb946729a7
Hernán Chumpitaz <hernan.chumpitaz@vallegrande.edu.pe>
2025-05-01 23:49:16 -0500

    foods

---
+++ src/main/java/pe/edu/vallegrande/foods/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package pe.edu.vallegrande.foods.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.addAllowedOrigin("https://4200-vallegrande-vgwebdashbo-qbguf9p9d3v.ws-us118.gitpod.io");
+        corsConfig.addAllowedMethod("*");
+        corsConfig.addAllowedHeader("*");
+        corsConfig.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
+}
