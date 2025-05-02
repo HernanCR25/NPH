3367cecdd3fb2a26600aa026f366d70655245aad
Hernán Chumpitaz <111783000+HernanCR25@users.noreply.github.com>
2025-05-02 09:40:37 +0000

    update

--- src/main/java/pe/edu/vallegrande/config/CorsConfig.java
+++ src/main/java/pe/edu/vallegrande/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.addAllowedOrigin("*");
+        corsConfig.setAllowedOrigins(Arrays.asList("https://orange-space-rotary-phone-r4644wj7r4xvcpq74-4200.app.github.dev", "https://nph-ciclodevida.onrender.com/cicloVida"));
         corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         corsConfig.setAllowCredentials(true);
